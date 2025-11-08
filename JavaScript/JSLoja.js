f1aaeea14cceea87372ffd512e59fcd07476c13e
Felipe Brumatti <felipebrumatti5@gmail.com>
2025-11-08 14:20:29 -0300

    Adição dos itens ao carrinho com funções

--- JavaScript/JSLoja.js
+++ JavaScript/JSLoja.js
@@ -31,8 +31,70 @@ function updatePrice(){
     totalPrice = totalPrice.replace(".", ",");
     document.getElementsByClassName("ValorFooter")[0].innerText = "R$" + totalPrice;
 }
+
 for(var i = 0; i < localStorage.length; i++){
     newItem = JSON.parse(localStorage.getItem("Cart"+i));
     ItensInCart.push(newItem)
 }
-console.log(ItensInCart)
+
+
+for(var i = 0; i < ItensInCart.length; i++){
+    const linkNewItem = ItensInCart[i].link;
+    const nameNewItem = ItensInCart[i].name;
+    const priceNewItem = ItensInCart[i].price;
+
+    let control = 0;
+ 
+    const oldItens = document.getElementsByClassName('imgCarrinho');
+    for(var j = 0; j < oldItens.length; j++){
+        if(oldItens[j].src == linkNewItem){
+            oldItens[j].parentElement.parentElement.querySelector('input').value++
+            control = 1;
+        }
+    }
+
+    if(control == 0){
+        let newDivProduct = document.createElement("div")
+        newDivProduct.classList.add("productDiv")
+        newDivProduct.innerHTML = 
+        `
+        <table>
+                <td>    
+                    <img src="${linkNewItem}" class="imgCarrinho" title="${nameNewItem}">
+                </td>
+                <td class="preçoCarrinho">
+                    ${priceNewItem}
+                </td>
+                <td class="qtdCarrinho">
+                    <input class="qtdInput" type="number" value="1" min="1">
+                </td>
+                <td>
+                    <button type="button" class="remover" style="font-family: Genshin; font-size:12px; color:white;">Remover</button>
+                </td>
+        </table>
+        `
+
+        let newFooter = document.createElement("div")
+        newFooter.classList.add("footer")
+        newFooter.innerHTML =
+        `
+        <table>
+                        <th> Valor Total: </th>
+                        <th> Quantidade de itens:</th>
+                        <th class="buttonFooter"><button type="button" class="confirmar" style="font-family: Genshin; font-size:12px; color:white;">Finalizar compra</button> </th> <tr>
+                        <td class="ValorFooter"> "SOMA"</td>
+                        <td class="QtdFooter"> "ITENS"</td>
+                    </table> 
+        </div>
+        ` 
+        document.getElementsByClassName('footer')[0].remove();
+        const mainDiv = document.getElementsByClassName("mainDiv")[0];
+        mainDiv.append(newDivProduct);
+        newDivProduct.getElementsByTagName('input')[0].addEventListener("change", updatePrice);
+        newDivProduct.getElementsByTagName('button')[0].addEventListener("click", removeItem);
+        mainDiv.append(newFooter)
+    }
+    updatePrice();
+}
+
+    
